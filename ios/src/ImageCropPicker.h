5375b015118e59cc04ae7ebb6a1b79481705405b
DineshSutar <dsutar720@gmail.com>
2019-09-23 11:10:08 +0530

    Implemented TOCropViewController to enhance image editing in IOS

--- ios/src/ImageCropPicker.h
+++ ios/src/ImageCropPicker.h
@@ -35,6 +35,8 @@
 #import <RSKImageCropper/RSKImageCropper.h>
 #endif
 
+#import <TOCropViewController/TOCropViewController.h>
+
 #import "UIImage+Resize.h"
 #import "Compression.h"
 #import <math.h>
@@ -45,7 +47,8 @@ UINavigationControllerDelegate,
 RCTBridgeModule,
 QBImagePickerControllerDelegate,
 RSKImageCropViewControllerDelegate,
-RSKImageCropViewControllerDataSource>
+RSKImageCropViewControllerDataSource,
+TOCropViewControllerDelegate>
 
 typedef enum selectionMode {
     CAMERA,
